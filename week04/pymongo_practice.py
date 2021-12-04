42393875fcd2d5fb99b74330a506fa97e3d9790e
mooyung <mooyung09@gmail.com>
2021-12-04 13:46:39 +0900

    mongodb update, delete 실습

--- week04/pymongo_practice.py
+++ week04/pymongo_practice.py
@@ -5,10 +5,17 @@ db = client.dbsparta # dbsparta 이름의 데이터베이스 생성 ( get or cre
 
 # adding users collection document
 # db.users.insert_one({'name': '덤블도어', 'age': 116})
+# all_users = list(db.users.find())
+# users = list(db.users.find({'age': 116}))
+# for user in users:
+#     print(user)
+# dumbledore = db.users.find_one({'age': 116})
+# print(dumbledore)
 
-all_users = list(db.users.find())
-users = list(db.users.find({'age': 116}))
-for user in users:
-    print(user)
-dumbledore = db.users.find_one({'age': 116})
-print(dumbledore)
+# 수정
+# 실무에서는 수정은 거의 안함
+db.users.update_one({'name': '덤블도어'}, {'$set': {'age': 19}})
+
+# 삭제
+# 실무에서 삭제는 더욱 안함
+db.users.delete_one({'name': '덤블도어'})

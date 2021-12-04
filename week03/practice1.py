a6b7abb3803d002a505d075f63a5c0e48894af78
mooyung <mooyung09@gmail.com>
2021-12-04 10:25:27 +0900

    Initial commit

---
+++ week03/practice1.py
@@ -0,0 +1,34 @@
+# hello, world!
+# no let
+# true / false
+boolean = True #False
+
+#console.log()
+print('hello')
+print('babo!!')
+
+# list, dictionary is same with JS
+num_list = [10,20,30,40] # num_list[0] = 10
+num_dict = {'num1':100, 'num2': 200} # num_dict['num1] -> 100
+
+# function
+def sum(num1, num2):
+    return num1 + num2
+
+result = sum(100, 200)
+print(result)
+
+# if () {} && -> and // || -> or
+if 10 < 20:
+    print('hahaha')
+
+numbers = [1,2,3]
+# for (int i=0, i < numbers.length ; i++) {
+# console.log(i)
+# }
+
+for number in numbers:
+    print(number)
+
+
+
